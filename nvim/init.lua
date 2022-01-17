4e077e0651da7f3df37027a3d4c12982756a10a6
Daniel Hill <danielhill10@bellsouth.net>
2022-01-17 01:34:56 -0500

    feat(nvim): Modify bash install config to use new init.lua script for Neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,24 +1,28 @@
+local has = function(feature)
+  return vim.fn.has(feature) == 1
+end
+
 -- Remap <leader> key to SPACE ----------------------------------------
 
-vim.g.mapleader = vim.api.nvim_replace_termcodes('<space>', true, true, true)
+vim.g.mapleader = vim.api.nvim_replace_termcodes("<space>", true, true, true)
 
 -- Editor configuration -----------------------------------------------
 
 -- Look and feel
 
-if vim.fn.has('termguicolors') == 1 then
+if has("termguicolors") then
   vim.o.termguicolors = true
 end
-vim.o.background = 'dark'
+vim.o.background = "dark"
 
 -- I don't want vim's status indicators for the various
 -- editor modes to visually conflict with airline's
-vim.o.noshowmode = true
+vim.o.showmode = false
 
 vim.o.number = true
 vim.o.relativenumber = true
 
-vim.o.guifont = 'CaskaydiaCove_NF:h14'
+vim.o.guifont = "CaskaydiaCove_NF:h14"
 
 vim.cmd([[
 syntax enable
@@ -36,44 +40,43 @@ vim.o.splitbelow = true
 
 -- Keymaps
 
-vim.api.nvim_set_keymap('', 'gf', ':edit <cfile><cr>')
+vim.api.nvim_set_keymap("", "gf", ":edit <cfile><cr>", {})
 
 -- Move split panes to left/bottom/top/right
-vim.api.nvim_set_keymap('n', '<A-h>', '<C-W>H', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-j>', '<C-W>J', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-k>', '<C-W>K', { noremap = true })
-vim.api.nvim_set_keymap('n', '<A-l>', '<C-W>L', { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-h>", "<C-W>H", { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-j>", "<C-W>J", { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-k>", "<C-W>K", { noremap = true })
+vim.api.nvim_set_keymap("n", "<A-l>", "<C-W>L", { noremap = true })
 
 -- Move between panes to left/bottom/top/right
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
 
 -- Vim-Plug -----------------------------------------------------------
 
+vim.g.nvim_config_dir = "~/.config/nvim"
+if has("win32") then
+  vim.g.nvim_config_dir = "~/AppData/Local/nvim"
+end
+
 -- TODO replace vim-plug with Packer
+vim.fn["plug#begin"](vim.g.nvim_config_dir .. "/plugged")
 vim.cmd([[
-if has('win32')
-  let g:nvim_config_dir = '~/AppData/Local/nvim'
-else
-  let g:nvim_config_dir = '~/.config/nvim'
-endif
-
-call plug#begin(g:nvim_config_dir . '/plugged')
-  execute "source" g:nvim_config_dir . '/plugins/gruvbox.vim'
-  execute "source" g:nvim_config_dir . '/plugins/vim-airline.vim'
-  execute "source" g:nvim_config_dir . '/plugins/vim-fugitive.vim'
-  execute "source" g:nvim_config_dir . '/plugins/fzf.vim'
-  execute "source" g:nvim_config_dir . '/plugins/auto-pairs.vim'
-  execute "source" g:nvim_config_dir . '/plugins/nerdtree.vim'
-  execute "source" g:nvim_config_dir . '/plugins/nerdcommenter.vim'
-  execute "source" g:nvim_config_dir . '/plugins/nvim-lspconfig.vim'
-  execute "source" g:nvim_config_dir . '/plugins/rust.vim'
-call plug#end()
-
-doautocmd User PlugLoaded
+execute "source" g:nvim_config_dir . '/plugins/gruvbox.vim'
+execute "source" g:nvim_config_dir . '/plugins/vim-airline.vim'
+execute "source" g:nvim_config_dir . '/plugins/vim-fugitive.vim'
+execute "source" g:nvim_config_dir . '/plugins/fzf.vim'
+execute "source" g:nvim_config_dir . '/plugins/auto-pairs.vim'
+execute "source" g:nvim_config_dir . '/plugins/nerdtree.vim'
+execute "source" g:nvim_config_dir . '/plugins/nerdcommenter.vim'
+execute "source" g:nvim_config_dir . '/plugins/nvim-lspconfig.vim'
+execute "source" g:nvim_config_dir . '/plugins/rust.vim'
 ]])
+vim.fn["plug#end"]()
+
+vim.cmd("doautocmd User PlugLoaded")
 
 -- nvim-lsp -----------------------------------------------------------
 
@@ -85,11 +88,11 @@ vim.cmd([[autocmd BufNewFile,BufRead *.rs set filetype=rust]])
 vim.g.rustfmt_autosave = 1
 
 -- I want as-you-type autocompletion
-vim.o.completeopt = { "menu", "menuone", "preview", "noselect", "noinsert" }
+vim.opt.completeopt = { "menu", "menuone", "preview", "noselect", "noinsert" }
 
 -- Color customization
 local brown = "905532"
-local aqua =  "3AFFDB"
+local aqua = "3AFFDB"
 local blue = "689FB6"
 local darkBlue = "44788E"
 local purple = "834F79"
