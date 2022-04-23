e1c38b6af4ccc771ed533ab058141bbe74615fac
Daniel Hill <danielhill10@bellsouth.net>
2022-04-22 21:41:33 -0400

    fix: use path to Python executable instead of searching on the Path

--- install.ps1
+++ install.ps1
@@ -6,17 +6,18 @@ $DOTBOT_DIR = "dotbot"
 $DOTBOT_BIN = "bin/dotbot"
 $BASEDIR = $PSScriptRoot
 
+$PYTHON = "$env:LOCALAPPDATA/Programs/Python/Python310/python.exe"
+
 Set-Location $BASEDIR
 git -C $DOTBOT_DIR submodule sync --quiet --recursive
 git submodule update --init --recursive $DOTBOT_DIR
 
-foreach ($PYTHON in ('python', 'python3', 'python2')) {
     # Python redirects to Microsoft Store in Windows 10 when not installed
-    if (& { $ErrorActionPreference = "SilentlyContinue"
-            ![string]::IsNullOrEmpty((&$PYTHON -V))
-            $ErrorActionPreference = "Stop" }) {
-        &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
-        return
-    }
+if (& { $ErrorActionPreference = "SilentlyContinue"
+        ![string]::IsNullOrEmpty((&$PYTHON -V))
+        $ErrorActionPreference = "Stop" }) {
+    &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG $Args
+    return
 }
+
 Write-Error "Error: Cannot find Python."
