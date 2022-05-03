cb0e3b564700f68c135dbcbaaa5695a35391efc7
Daniel Hill <danielhill10@bellsouth.net>
2022-05-03 00:06:48 -0400

    chore(nvim): update nvim-tree options according to https://github.com/kyazdani42/nvim-tree.lua/issues/674

--- nvim/after/plugin/nvim_tree.lua
+++ nvim/after/plugin/nvim_tree.lua
@@ -21,23 +21,66 @@ end
 
 -- local tree_cb = nvim_tree_config.nvim_tree_callback
 
--- following options are the default
+-- setup with all defaults
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-nvim_tree.setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = {},
-	auto_close = false,
-	open_on_tab = false,
+nvim_tree.setup({ -- BEGIN_DEFAULT_OPTS
+	auto_reload_on_write = true,
+	disable_netrw = false,
 	hijack_cursor = false,
+	hijack_netrw = true,
+	hijack_unnamed_buffer_when_opening = false,
+	ignore_buffer_on_setup = false,
+	open_on_setup = false,
+	open_on_setup_file = false,
+	open_on_tab = false,
+	sort_by = "name",
 	update_cwd = false,
-	update_to_buf_dir = {
+	view = {
+		width = 30,
+		height = 30,
+		hide_root_folder = false,
+		side = "left",
+		preserve_window_proportions = false,
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+		mappings = {
+			custom_only = false,
+			list = {
+				-- user mappings go here
+			},
+		},
+	},
+	renderer = {
+		indent_markers = {
+			enable = false,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+		},
+	},
+	hijack_directories = {
 		enable = true,
 		auto_open = true,
 	},
+	update_focused_file = {
+		enable = false,
+		update_cwd = false,
+		ignore_list = {},
+	},
+	ignore_ft_on_setup = {},
+	system_open = {
+		cmd = "",
+		args = {},
+	},
 	diagnostics = {
 		enable = false,
+		show_on_dirs = false,
 		icons = {
 			hint = "",
 			info = "",
@@ -45,40 +88,50 @@ nvim_tree.setup({
 			error = "",
 		},
 	},
-	update_focused_file = {
-		enable = false,
-		update_cwd = false,
-		ignore_list = {},
-	},
-	system_open = {
-		cmd = nil,
-		args = {},
-	},
 	filters = {
 		dotfiles = false,
 		custom = {},
+		exclude = {},
 	},
 	git = {
 		enable = true,
 		ignore = true,
-		timeout = 500,
+		timeout = 400,
 	},
-	view = {
-		width = 30,
-		height = 30,
-		hide_root_folder = false,
-		side = "left",
-		auto_resize = false,
-		mappings = {
-			custom_only = false,
-			list = {},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = true,
+			restrict_above_cwd = false,
 		},
-		number = false,
-		relativenumber = false,
-		signcolumn = "yes",
+		open_file = {
+			quit_on_open = true,
+			resize_window = false,
+			window_picker = {
+				enable = false,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", "vim-plug" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
 	},
 	trash = {
 		cmd = "trash",
 		require_confirm = true,
+	},
+	log = {
+		enable = false,
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
+		},
 	},
 })
--- nvim/lua/blaz/globals/nvim_tree.lua
+++ nvim/lua/blaz/globals/nvim_tree.lua
@@ -1,34 +1,16 @@
 -- Find the nvim-tree global options here:
 -- https://github.com/kyazdani42/nvim-tree.lua#setup
 
-vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
-vim.g.nvim_tree_indent_markers = 0 -- 0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_git_hl = 0 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filename-modifiers for more options
 vim.g.nvim_tree_add_trailing = 0 -- 0 by default, append a trailing slash to folder names
 vim.g.nvim_tree_group_empty = 0 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g.nvim_tree_change_dir_global = 1 -- 0 by default, use :cd when changing directories.
-vim.g.nvim_tree_disable_window_picker = 1 -- 0 by default, will disable the window picker.
 vim.g.nvim_tree_icon_padding = " " -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
 vim.g.nvim_tree_symlink_arrow = " >> " --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
 vim.g.nvim_tree_respect_buf_cwd = 0 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 vim.g.nvim_tree_create_in_closed_folder = 0 -- 1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
 vim.g.nvim_tree_refresh_wait = 500 -- 1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
-
--- Dictionary of buffer option names mapped to a list of option values that
--- indicates to the window picker that the buffer's window should not be
--- selectable.
-vim.g.nvim_tree_window_picker_exclude = {
-	["filetype"] = {
-		"notify",
-		"packer",
-		"qf",
-	},
-	["buftype"] = {
-		"terminal",
-	},
-}
 
 -- List of filenames that gets highlighted with NvimTreeSpecialFile
 vim.g.nvim_tree_special_files = {
