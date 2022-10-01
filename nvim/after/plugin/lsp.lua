03c8a178675a22f9aff61556308bc94794e643b9
Daniel Hill <danielhill10@bellsouth.net>
2022-10-01 19:52:42 -0400

    refactor: move lsp configurations

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,38 @@
+local notify = require("blaz.helper.notify")
+
+local has_lspconfig, lspconfig = pcall(require, "lspconfig")
+if not has_lspconfig then
+	notify.warn(
+		"LSP",
+		"lspconfig not found!",
+		"Skipping configuration for this plugin...",
+		"Some features may not work properly..."
+	)
+	return
+end
+
+local has_rust_tools, rust_tools = pcall(require, "rust-tools")
+if not has_rust_tools then
+	notify.warn(
+		"LSP (Rust)",
+		"rust-tools not found!",
+		"Skipping configuration for this plugin...",
+		"Some features may not work properly..."
+	)
+	return
+end
+
+local opts = require("blaz.lsp.opts")
+local default_opts = require("blaz.lsp.opts.defaults")
+
+lspconfig.omnisharp.setup(opts.omnisharp)
+
+lspconfig.sumneko_lua.setup(opts.sumneko_lua)
+
+lspconfig.jsonls.setup(opts.jsonls)
+
+rust_tools.setup({ server = default_opts })
+
+lspconfig.clangd.setup(default_opts)
+
+lspconfig.yamlls.setup(default_opts)
