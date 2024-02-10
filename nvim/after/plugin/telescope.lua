bcb749339988d714a3106745e5af4a01651a925e
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-02-10 11:59:36 -0500

    refactor(nvim): configure `telescope.nvim` via `lazy.nvim`

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,4 @@
 local notify = require("blaz.helper.notify")
-local telescope_opts = require("blaz.plugins.opts.telescope")
 
 local has_telescope, telescope = pcall(require, "telescope")
 if not has_telescope then
@@ -12,85 +11,5 @@ if not has_telescope then
 	return
 end
 
--- local has_fzy_native, _ = pcall(require, "fzy_native")
--- if not has_fzy_native then
--- notify.debug(
--- 	"Fuzzy Finder",
--- 	"telescope-fzy-native not found!",
--- 	"Will load nvim-telescope without it..."
--- )
--- end
-
--- local has_ui_select, _ = pcall(require, "ui-select")
--- if not has_ui_select then
--- notify.debug(
--- 	"Fuzzy Finder",
--- 	"telescope-ui-select not found!",
--- 	"Will load nvim-telescope without it..."
--- )
--- end
-
-telescope.setup(telescope_opts)
-
-pcall(telescope.load_extension, "noice")
 pcall(telescope.load_extension, "fzy_native")
--- pcall(telescope.load_extension, "file_browser")
 pcall(telescope.load_extension, "ui-select")
-
-vim.keymap.set(
-	"n",
-	"<leader>ff",
-	require("blaz.helper.telescope").project_files,
-	{ desc = "[F]ind [F]iles" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fc",
-	require("telescope.builtin").colorscheme,
-	{ desc = "[F]ind [C]olorscheme" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fd",
-	require("telescope.builtin").diagnostics,
-	{ desc = "[F]ind [D]iagnostics" }
-)
--- Need ripgrep installed for the `live-grep` functionality
-vim.keymap.set(
-	"n",
-	"<leader>fg",
-	require("telescope.builtin").live_grep,
-	{ desc = "[F]ind by [G]rep" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fw",
-	require("telescope.builtin").grep_string,
-	{ desc = "[F]ind current [W]ord" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fv",
-	require("telescope.builtin").git_branches,
-	{ desc = "[F]ind Git [V]branches" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fb",
-	require("telescope.builtin").buffers,
-	{ desc = "[F]ind [B]uffers" }
-)
-vim.keymap.set(
-	"n",
-	"<leader>fh",
-	require("telescope.builtin").help_tags,
-	{ desc = "[F]ind [H]elp tags" }
-)
--- vim.keymap.set("n", "<C-n>", "<cmd>Telescope file_browser<CR>", { remap = false })
-vim.keymap.set("n", "<leader>/", function()
-	-- You can pass additional configuration to telescope to change theme, layout, etc.
-	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-		winblend = 10,
-		previewer = false,
-	}))
-end, { desc = "[/] Fuzzily search in current buffer" })
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -8,6 +8,96 @@ return {
 			"nvim-telescope/telescope-fzy-native.nvim",
 			"nvim-telescope/telescope-ui-select.nvim",
 		},
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope git_files<cr>",   desc = "[F]ind [F]iles" },
+			{ "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "[F]ind [C]olorscheme" },
+			{ "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "[F]ind [D]iagnostics" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>",   desc = "[F]ind by [G]rep" },
+			{ "<leader>fw", "<cmd>Telescope grep_string<cr>", desc = "[F]ind current [W]ord" },
+			{
+				"<leader>fv",
+				"<cmd>Telescope git_branches<cr>",
+				desc = "[F]ind Git [V]branches",
+			},
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>",   desc = "[F]ind [B]uffers" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "[F]ind [H]elp tags" },
+			-- {
+			-- 	"<leader>/",
+			-- 	function()
+			-- 		require("telescope.builtin").current_buffer_fuzzy_find(
+			-- 			require("telescope.themes").get_dropdown({
+			-- 				winblend = 10,
+			-- 				previewer = false,
+			-- 			})
+			-- 		)
+			-- 	end,
+			-- 	desc = "[/] Fuzzily search in current buffer",
+			-- },
+		},
+		opts = {
+			defaults = {
+				mappings = {
+					i = {
+						["<C-n>"] = false,
+						["<C-p>"] = false,
+						["<C-j>"] = "move_selection_next",
+						["<C-k>"] = "move_selection_previous",
+					},
+				},
+				layout_config = {
+					prompt_position = "top",
+				},
+				prompt_prefix = "🔍 ",
+				selection_caret = "👉 ",
+				entry_prefix = "  ",
+				selection_strategy = "reset",
+				sorting_strategy = "descending",
+				scroll_strategy = "cycle",
+				color_devicons = true,
+			},
+			pickers = {
+				colorscheme = {
+					theme = "cursor",
+					previewer = false,
+					prompt_prefix = "🎨 ",
+				},
+				find_files = {
+					theme = "dropdown",
+					previewer = false,
+				},
+				git_files = {
+					theme = "dropdown",
+					previewer = false,
+				},
+				git_branches = {
+					theme = "dropdown",
+					prompt_prefix = " ",
+				},
+				git_commits = {
+					prompt_prefix = " ",
+				},
+				buffer = {
+					theme = "dropdown",
+					prompt_prefix = "﬘ ",
+				},
+				codeaction = {
+					theme = "cursor",
+				},
+				help_tags = {
+					theme = "ivy",
+					prompt_prefix = "⁉ ",
+				},
+				planets = {
+					prompt_prefix = "🌎 ",
+				},
+			},
+			extensions = {
+				fzy_native = {
+					override_generic_sorter = false,
+					override_file_sorter = true,
+				},
+			},
+		},
 	},
 	{
 		"j-hui/fidget.nvim",
